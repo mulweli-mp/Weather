9905b0ba99a1bafe71fdb13e2d9515dceed5a805
GeneCodeFx <mulwelimushiana@yahoo.com>
2023-10-31 01:34:39 +0200

    Added StatusBar

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import Navigator from "./src/navigation/Navigator";
 import { ThemeProvider } from "./src/context/ThemeContext";
+import { StatusBar } from "expo-status-bar";
 
 function App() {
   return (
     <ThemeProvider>
       <Navigator />
+      <StatusBar style="light" />
     </ThemeProvider>
   );
 }
