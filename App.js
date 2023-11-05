f44e02d072e7416789c581a64f227f063bec2778
GeneCodeFx <mulwelimushiana@yahoo.com>
2023-11-05 14:33:41 +0200

    Included StatusBar

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import Navigator from "./src/navigation/Navigator";
 import { ThemeProvider } from "./src/context/ThemeContext";
 import { UserProvider } from "./src/context/UserContext";
+import { StatusBar } from "expo-status-bar";
 
 function App() {
   return (
     <UserProvider>
       <ThemeProvider>
         <Navigator />
+        <StatusBar style="light" />
       </ThemeProvider>
     </UserProvider>
   );
