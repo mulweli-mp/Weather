fbb99e44b20928ff09916f610f353e9220d36e38
GeneCodeFx <mulwelimushiana@yahoo.com>
2023-11-05 14:37:06 +0200

    Added full support for offline mode

--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -1,34 +1,74 @@
-import { useState, createContext } from "react";
+import { useState, createContext, useEffect } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export const UserContext = createContext();
 
 export const UserProvider = (props) => {
   const [userWeatherData, setUserWeatherData] = useState({
-    currentLocationWeather: {},
+    currentLocationWeather: {
+      today: {},
+      forecast5Days: [],
+    },
     savedLocationsWeather: [],
   });
 
-  const updateUserWeatherData = (fetchOrigion, newData) => {
-    let newWeatherData;
-    if (fetchOrigion === "currentLocation") {
-      newWeatherData = {
-        ...userWeatherData,
-        ...{
-          currentLocationWeather: newData,
-        },
-      };
-    } else {
-      let savedLocations = userWeatherData.savedLocationsWeather;
-      savedLocations.push(newData);
+  useEffect(() => {
+    getOfflineWeatherData();
+  }, []);
 
-      newWeatherData = {
-        ...userWeatherData,
-        ...{
-          savedLocationsWeather: savedLocations,
-        },
-      };
+  const getOfflineWeatherData = async () => {
+    console.log(`Running getOfflineWeatherData`);
+    try {
+      const jsonValue = await AsyncStorage.getItem("weather-data");
+      const savedData = jsonValue != null ? JSON.parse(jsonValue) : null;
+      if (savedData) {
+        setUserWeatherData(savedData);
+      }
+    } catch (e) {
+      // error reading value
+      alert(e);
     }
+  };
+
+  const placeAlreadySaved = (savedLocations, valueToCheck) => {
+    return savedLocations.some(
+      (object) => object["placeName"] === valueToCheck
+    );
+  };
+
+  const updateUserWeatherData = (newData) => {
+    const { today, forecast5Days } = newData;
+    const { placeName, latitude, longitude } = today;
+
+    let savedLocations = userWeatherData.savedLocationsWeather;
+    if (!placeAlreadySaved(savedLocations, placeName)) {
+      let savedLocations = userWeatherData.savedLocationsWeather;
+      savedLocations.push({
+        placeName,
+        latitude,
+        longitude,
+      });
+    }
+
+    let newWeatherData = {
+      currentLocationWeather: {
+        today,
+        forecast5Days,
+      },
+      savedLocationsWeather: savedLocations,
+    };
+
     setUserWeatherData(newWeatherData);
+    storeOfflineWeatherData(newWeatherData);
+  };
+
+  const storeOfflineWeatherData = async (value) => {
+    try {
+      const jsonValue = JSON.stringify(value);
+      await AsyncStorage.setItem("weather-data", jsonValue);
+    } catch (e) {
+      // saving error
+    }
   };
 
   return (
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -6,9 +6,11 @@ import {
   ImageBackground,
   Modal,
   TouchableOpacity,
+  Alert,
+  Vibration,
 } from "react-native";
 import { OPEN_WEATHER_API_KEY } from "@env";
-import { StatusBar } from "expo-status-bar";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 import { ThemeContext, UserContext } from "../context";
 import {
@@ -18,6 +20,7 @@ import {
   LoadingAnimation,
   LocationPermision,
   EditLocation,
+  DisplayError,
 } from "../components";
 
 export default function Home({ navigation }) {
@@ -26,46 +29,53 @@ export default function Home({ navigation }) {
   const [isLoadingCurrent, setIsLoadingCurrent] = useState(true);
   const [isLoadingForecast, setIsLoadingForecast] = useState(true);
   const [currentWeather, setCurrentWeather] = useState(null);
-  const [currentTemperature, setCurrentTemperature] = useState(15);
-  const [minTemperature, setMinTemperature] = useState(10);
-  const [maxTemperature, setMaxTemperature] = useState(17);
   const [forecastArray, setForecastArray] = useState([]);
-  const [locationName, setLocationName] = useState("Your location");
-
   const [locationModalVisible, setLocationModalVisible] = useState(false);
-
   const [isFetchingLocation, setIsFetchingLocation] = useState(true);
   const [lastUpdatedTime, setlastUpdatedTime] = useState("Just Now");
+  const [errorMessage, setErrorMessage] = useState(null);
 
   useEffect(() => {
+    syncUpdateTime();
     const intervalId = setInterval(() => {
-      const minutesDifference = calculateMinutesDifference(
-        currentWeather?.timeUpdated,
-        new Date()
-      );
-
-      if (minutesDifference < 2) {
-        setlastUpdatedTime("Just Now");
-      } else if (minutesDifference > 2 && minutesDifference < 60) {
-        setlastUpdatedTime(`${parseInt(minutesDifference)} mins ago`);
-      } else {
-        const hoursDifference = minutesDifference / 60;
-
-        const wording =
-          parseInt(hoursDifference) == 1
-            ? "an hour ago"
-            : `${parseInt(hoursDifference)} hours ago`;
-        setlastUpdatedTime(wording);
-      }
-
-      console.log(`The difference in minutes is: ${minutesDifference} minutes`);
-    }, 1000);
+      syncUpdateTime();
+    }, 30000);
 
     // Clear the interval when the component unmounts
     return () => {
       clearInterval(intervalId);
     };
   }, [currentWeather]);
+
+  const syncUpdateTime = () => {
+    if (currentWeather) {
+      const minutesDifference = calculateMinutesDifference(
+        currentWeather?.timeUpdated,
+        new Date()
+      );
+
+      let lastUpdateString = "";
+
+      if (minutesDifference < 2) {
+        lastUpdateString = "Just Now";
+      } else if (minutesDifference > 2 && minutesDifference < 60) {
+        lastUpdateString = `${parseInt(minutesDifference)} mins ago`;
+      } else {
+        const hoursDifference = minutesDifference / 60;
+        if (hoursDifference < 24) {
+          lastUpdateString =
+            parseInt(hoursDifference) == 1
+              ? "an hour ago"
+              : `${parseInt(hoursDifference)} hours ago`;
+        } else {
+          const dateUpdated = new Date(currentWeather?.timeUpdated);
+          lastUpdateString = `${dateUpdated}`.substring(0, 21);
+        }
+      }
+
+      setlastUpdatedTime(lastUpdateString);
+    }
+  };
 
   const calculateMinutesDifference = (timestamp1, timestamp2) => {
     const date1 = new Date(timestamp1);
@@ -80,159 +90,210 @@ export default function Home({ navigation }) {
     return minutesDifference;
   };
 
-  const fetchWeatherData = async (
+  const processCurrentWeatherData = (data) => {
+    const {
+      main,
+      weather,
+      name,
+      latitude,
+      longitude,
+      fetchOrigion,
+      searchQueryPlaceName,
+    } = data;
+    const { temp, temp_min, temp_max } = main;
+    const { main: weatherMain, description } = weather[0];
+
+    const general = weatherCategories[weatherMain] || "unknown";
+    const placeName = searchQueryPlaceName ? searchQueryPlaceName : name;
+
+    const currentTemperature = Math.round(temp);
+    const minTemperature = Math.round(temp_min);
+    const maxTemperature = Math.round(temp_max);
+
+    const timeUpdated = new Date();
+
+    const weatherData = {
+      general,
+      main: weatherMain,
+      description,
+      placeName,
+      currentTemperature,
+      latitude,
+      longitude,
+      fetchOrigion,
+      timeUpdated,
+      minTemperature,
+      maxTemperature,
+    };
+
+    setCurrentWeather(weatherData);
+    setIsLoadingCurrent(false);
+  };
+
+  const processFiveDayWeatherForecast = (data) => {
+    // Extract relevant data for the next 5 days
+    const forecastList = data.list;
+    const fiveDayForecast = [];
+
+    // Get today's date and initialize a variable for the day of the week
+    let currentDate = new Date();
+    let currentDay = currentDate.getDay();
+    const today = currentDay;
+
+    // Define an array of day names for formatting
+    const dayNames = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+
+    let minTemp;
+    let maxTemp;
+    for (const forecast of forecastList) {
+      const date = new Date(forecast.dt * 1000); // Convert timestamp to date
+      const dayOfWeek = dayNames[date.getDay()];
+
+      if (date.getDay() === today) {
+        //Do nothing and move to the next day
+      } else if (date.getDay() !== currentDay) {
+        // It's a new day, add the forecast to the result
+        const dayForecast = forecast.weather[0].main;
+        const weather = weatherCategories[dayForecast] || "unknown";
+
+        const { temp } = forecast.main;
+        minTemp = temp;
+        maxTemp = temp;
+
+        fiveDayForecast.push({
+          day: dayOfWeek,
+          weather,
+          temperature: {
+            temp_min: Math.round(minTemp),
+            temp_max: Math.round(maxTemp),
+          },
+        });
+
+        // Move to the next day
+        currentDay = date.getDay();
+      } else {
+        //Basically, we loop through all the results of the same day
+        //get the lowest and highest temperatures at different times of the day
+        //and use the lowest as temp_min and the highest as temp_max
+
+        const { temp } = forecast.main;
+        minTemp = temp < minTemp ? temp : minTemp;
+        maxTemp = temp > maxTemp ? temp : maxTemp;
+
+        const lastPushedObjIndex = fiveDayForecast.length - 1;
+
+        fiveDayForecast[lastPushedObjIndex].temperature = {
+          temp_min: Math.round(minTemp),
+          temp_max: Math.round(maxTemp),
+        };
+      }
+    }
+
+    setForecastArray(fiveDayForecast);
+    setIsLoadingForecast(false);
+  };
+
+  const fetchWeatherForecast = async (
     latitude,
     longitude,
     fetchOrigion,
     searchQueryPlaceName
   ) => {
-    const apiUrl = "http://api.openweathermap.org/data/2.5/weather";
+    console.log(`Running fetchWeatherForecast`);
+
+    const currentWeatherApiUrl =
+      "http://api.openweathermap.org/data/2.5/weather";
+
+    const forecastApiUrl = "http://api.openweathermap.org/data/2.5/forecast";
+
     try {
       setIsLoadingCurrent(true);
+      setIsLoadingForecast(true);
+      setIsFetchingLocation(false);
 
-      const response = await fetch(
-        `${apiUrl}?lat=${latitude}&lon=${longitude}&appid=${OPEN_WEATHER_API_KEY}&units=metric`
+      //Fetch data for current weather
+      const currentWeatherResponse = await fetch(
+        `${currentWeatherApiUrl}?lat=${latitude}&lon=${longitude}&appid=${OPEN_WEATHER_API_KEY}&units=metric`
       );
 
-      if (!response.ok) {
-        console.warn("Network response was not ok");
+      if (!currentWeatherResponse.ok) {
+        setErrorMessage(
+          "Something went wrong fetching current weather data. Please try again later"
+        );
       }
 
-      const data = await response.json();
-
-      const { main, weather, name } = data;
-      const { temp, temp_min, temp_max } = main;
-      const { main: weatherMain, description } = weather[0];
-
-      const general = weatherCategories[weatherMain] || "unknown";
-      const placeName = searchQueryPlaceName ? searchQueryPlaceName : name;
-      const currentTemp = Math.round(temp);
-      const timeUpdated = new Date();
-
-      setCurrentTemperature(currentTemp);
-      setMaxTemperature(Math.round(temp_max));
-      setMinTemperature(Math.round(temp_min));
-
-      const weatherData = {
-        general,
-        main: weatherMain,
-        description,
-        placeName,
-        currentTemp,
+      const currentWeatherData = await currentWeatherResponse.json();
+      processCurrentWeatherData({
+        ...currentWeatherData,
         latitude,
         longitude,
         fetchOrigion,
-        timeUpdated,
-      };
+        searchQueryPlaceName,
+      });
 
-      setCurrentWeather(weatherData);
-      setLocationName(placeName);
-      updateUserWeatherData(fetchOrigion, weatherData);
-      setIsLoadingCurrent(false);
-    } catch (error) {
-      console.error("Error fetching weather data:", error);
-      setIsLoadingCurrent(false);
-    }
-  };
+      //Fetch data for future weather forecast
 
-  const fetchFiveDayWeatherForecast = async (latitude, longitude) => {
-    const apiUrl = "http://api.openweathermap.org/data/2.5/forecast";
-    try {
-      setIsLoadingCurrent(true);
-      const response = await fetch(
-        `${apiUrl}?lat=${latitude}&lon=${longitude}&appid=${OPEN_WEATHER_API_KEY}&units=metric`
+      const forecastWeatherResponse = await fetch(
+        `${forecastApiUrl}?lat=${latitude}&lon=${longitude}&appid=${OPEN_WEATHER_API_KEY}&units=metric`
       );
 
-      if (!response.ok) {
-        console.warn("Network response was not ok");
+      if (!forecastWeatherResponse.ok) {
+        setErrorMessage(
+          "Something went wrong fetching future forecast weather data. Please try again later"
+        );
       }
 
-      const data = await response.json();
-
-      // Extract relevant data for the next 5 days
-      const forecastList = data.list;
-      const fiveDayForecast = [];
-
-      // Get today's date and initialize a variable for the day of the week
-      let currentDate = new Date();
-      let currentDay = currentDate.getDay();
-      const today = currentDay;
-
-      // Define an array of day names for formatting
-      const dayNames = [
-        "Sunday",
-        "Monday",
-        "Tuesday",
-        "Wednesday",
-        "Thursday",
-        "Friday",
-        "Saturday",
-      ];
-
-      let minTemp;
-      let maxTemp;
-      for (const forecast of forecastList) {
-        const date = new Date(forecast.dt * 1000); // Convert timestamp to date
-        const dayOfWeek = dayNames[date.getDay()];
-
-        if (date.getDay() === today) {
-          //Do nothing and move to the next day
-        } else if (date.getDay() !== currentDay) {
-          // It's a new day, add the forecast to the result
-          const dayForecast = forecast.weather[0].main;
-          const weather = weatherCategories[dayForecast] || "unknown";
-
-          const { temp } = forecast.main;
-          minTemp = temp;
-          maxTemp = temp;
-
-          fiveDayForecast.push({
-            day: dayOfWeek,
-            weather,
-            temperature: {
-              temp_min: Math.round(minTemp),
-              temp_max: Math.round(maxTemp),
-            },
-          });
-
-          // Move to the next day
-          currentDay = date.getDay();
-        } else {
-          //Basically, we loop through all the results of the same day
-          //get the lowest and highest temperatures at different times of the day
-          //and use the lowest as temp_min and the highest as temp_max
-
-          const { temp } = forecast.main;
-          minTemp = temp < minTemp ? temp : minTemp;
-          maxTemp = temp > maxTemp ? temp : maxTemp;
-
-          const lastPushedObjIndex = fiveDayForecast.length - 1;
-
-          fiveDayForecast[lastPushedObjIndex].temperature = {
-            temp_min: Math.round(minTemp),
-            temp_max: Math.round(maxTemp),
-          };
-        }
-      }
-      // console.log(`fiveDayForecast:`, fiveDayForecast);
-
-      setForecastArray(fiveDayForecast);
-      setIsLoadingForecast(false);
+      const forecastWeatherData = await forecastWeatherResponse.json();
+      processFiveDayWeatherForecast(forecastWeatherData);
     } catch (error) {
-      console.error("Error fetching weather data:", error);
-      setIsLoadingForecast(false);
+      getOfflineWeatherData(error.message);
     }
   };
 
-  const fetchWeatherForecast = (
-    latitude,
-    longitude,
-    fetchOrigion,
-    searchQueryPlaceName
-  ) => {
-    setIsFetchingLocation(false);
-    fetchWeatherData(latitude, longitude, fetchOrigion, searchQueryPlaceName);
-    fetchFiveDayWeatherForecast(latitude, longitude);
+  const getOfflineWeatherData = async (errorText) => {
+    console.log(`Running getOfflineWeatherData`);
+    try {
+      const jsonValue = await AsyncStorage.getItem("weather-data");
+      const savedData = jsonValue != null ? JSON.parse(jsonValue) : null;
+      if (savedData) {
+        //Previously saved data is available
+        const { today, forecast5Days } = savedData.currentLocationWeather;
+        setForecastArray(forecast5Days);
+        setCurrentWeather(today);
+        setIsLoadingCurrent(false);
+        setIsLoadingForecast(false);
+
+        const errorTitle =
+          errorText == "Network request failed"
+            ? "No internet connection!"
+            : "Something went wrong!";
+
+        const errorBody =
+          errorText == "Network request failed"
+            ? "Please note that the displayed weather information may be outdated"
+            : errorText;
+
+        Alert.alert(errorTitle, errorBody, [
+          {
+            text: "Noted",
+            style: "cancel",
+          },
+        ]);
+        Vibration.vibrate();
+      } else {
+        setErrorMessage(errorText);
+      }
+    } catch (e) {
+      setErrorMessage(e.message);
+    }
   };
 
   const weatherCategories = {
@@ -259,6 +320,17 @@ export default function Home({ navigation }) {
     },
   };
 
+  if (errorMessage) {
+    return (
+      <DisplayError
+        errorMessage={errorMessage}
+        tryAgain={() => {
+          setIsFetchingLocation(true);
+          setErrorMessage(null);
+        }}
+      />
+    );
+  }
   if (isFetchingLocation) {
     return <LocationPermision fetchWeatherForecast={fetchWeatherForecast} />;
   }
@@ -275,9 +347,13 @@ export default function Home({ navigation }) {
           backgroundColor: themeColors[currentWeather.general],
         },
       ]}
+      onLayout={() => {
+        updateUserWeatherData({
+          today: currentWeather,
+          forecast5Days: forecastArray,
+        });
+      }}
     >
-      <StatusBar style="light" />
-
       <ImageBackground
         style={styles.currentWeatherContainer}
         source={weatherImages[themeColors.theme][currentWeather.general]}
@@ -285,10 +361,12 @@ export default function Home({ navigation }) {
       >
         <HomeHeader
           openDrawer={() => navigation.openDrawer()}
-          locationName={locationName}
+          placeName={currentWeather.placeName}
           setLocationModalVisible={setLocationModalVisible}
         />
-        <Text style={styles.temperatureText}>{currentTemperature}°C</Text>
+        <Text style={styles.temperatureText}>
+          {currentWeather.currentTemperature}°C
+        </Text>
         <Text style={styles.weatherDescriptionText}>
           {currentWeather.description.toUpperCase()}
         </Text>
@@ -312,11 +390,7 @@ export default function Home({ navigation }) {
         </View>
       </ImageBackground>
       <View style={styles.forecastContainer}>
-        <TodaysWather
-          minTemperature={minTemperature}
-          currentTemperature={currentTemperature}
-          maxTemperature={maxTemperature}
-        />
+        <TodaysWather currentWeather={currentWeather} />
         <ForecastWeather forecastArray={forecastArray} />
       </View>
 
