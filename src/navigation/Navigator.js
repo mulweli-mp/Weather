51d014ca1881a576b7c984eb98e68ea9ffd5a496
GeneCodeFx <mulwelimushiana@yahoo.com>
2024-05-19 09:20:02 +0200

    Added PlayAudio and StreamAudio

--- src/navigation/Navigator.js
+++ src/navigation/Navigator.js
@@ -4,6 +4,8 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import DrawerNavigation from "./DrawerNavigation";
 import Settings from "../screens/Settings";
 import SavedLocations from "../screens/SavedLocations";
+import PlayAudio from "../screens/PlayAudio";
+import StreamAudio from "../screens/StreamAudio";
 
 const Stack = createNativeStackNavigator();
 
@@ -11,7 +13,7 @@ function Navigator() {
   return (
     <NavigationContainer>
       <Stack.Navigator
-        initialRouteName="DrawerNavigation"
+        initialRouteName="PlayAudio"
         screenOptions={{
           headerShown: false,
         }}
@@ -19,6 +21,8 @@ function Navigator() {
         <Stack.Screen name="DrawerNavigation" component={DrawerNavigation} />
         <Stack.Screen name="Settings" component={Settings} />
         <Stack.Screen name="SavedLocations" component={SavedLocations} />
+        <Stack.Screen name="PlayAudio" component={PlayAudio} />
+        <Stack.Screen name="StreamAudio" component={StreamAudio} />
       </Stack.Navigator>
     </NavigationContainer>
   );
