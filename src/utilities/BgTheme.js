5522fdde3bd3b28142b9a67a740a5dd295dc81ca
GeneCodeFx <mulwelimushiana@yahoo.com>
2024-05-21 22:36:49 +0200

    bgTheme images and colours

---
+++ src/utilities/BgTheme.js
@@ -0,0 +1,34 @@
+export default bgTheme = {
+  day: {
+    clear: {
+      image: require("../../assets/Images/wallpapers/day-clear.png"),
+      colour: "#74C0F7",
+    },
+    cloudy: {
+      image: require("../../assets/Images/wallpapers/day-cloudy.png"),
+      colour: "#4D5762",
+    },
+    rainy: {
+      image: require("../../assets/Images/wallpapers/day-rainy.png"),
+      colour: "#325759",
+    },
+    sunny: {
+      image: require("../../assets/Images/wallpapers/sunny.png"),
+      colour: "#633C0F",
+    },
+  },
+  night: {
+    clear: {
+      image: require("../../assets/Images/wallpapers/night-clear.png"),
+      colour: "#060606",
+    },
+    cloudy: {
+      image: require("../../assets/Images/wallpapers/night-cloudy.png"),
+      colour: "#05101C",
+    },
+    rainy: {
+      // image: require("../../assets/Images/wallpapers/night-rainy.png"),
+      colour: "#00000",
+    },
+  },
+};
