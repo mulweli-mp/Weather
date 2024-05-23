653a74270f6a735c5cc3b4895369bfc9fa227fbf
GeneCodeFx <mulwelimushiana@yahoo.com>
2024-05-23 20:08:58 +0200

    completed themes pictures and colours for all weather conditions

--- src/utilities/BgTheme.js
+++ src/utilities/BgTheme.js
@@ -1,34 +1,229 @@
 export default bgTheme = {
-  day: {
-    clear: {
-      image: require("../../assets/Images/wallpapers/day-clear.png"),
-      colour: "#74C0F7",
-    },
-    cloudy: {
-      image: require("../../assets/Images/wallpapers/day-cloudy.png"),
-      colour: "#4D5762",
-    },
-    rainy: {
-      image: require("../../assets/Images/wallpapers/day-rainy.png"),
-      colour: "#325759",
-    },
-    sunny: {
-      image: require("../../assets/Images/wallpapers/sunny.png"),
+  naturesCanvas: {
+    // "01d": {
+    //   image: require("../../assets/Images/forest/sunny.png"),
+    //   colour: "#47ab2f",
+    // },
+    "01d": {
+      image: require("../../assets/Images/wallpapers/01d.png"),
       colour: "#633C0F",
     },
-  },
-  night: {
-    clear: {
-      image: require("../../assets/Images/wallpapers/night-clear.png"),
+    "01n": {
+      image: require("../../assets/Images/wallpapers/01n.png"),
       colour: "#060606",
     },
-    cloudy: {
-      image: require("../../assets/Images/wallpapers/night-cloudy.png"),
+    "02d": {
+      image: require("../../assets/Images/wallpapers/02d.png"),
+      colour: "#74C0F7",
+    },
+    "02n": {
+      image: require("../../assets/Images/wallpapers/02n.png"),
       colour: "#05101C",
     },
-    rainy: {
-      // image: require("../../assets/Images/wallpapers/night-rainy.png"),
-      colour: "#00000",
+    "03d": {
+      image: require("../../assets/Images/wallpapers/03d.png"),
+      colour: "#659BD7",
+    },
+    "03n": {
+      image: require("../../assets/Images/wallpapers/03n.png"),
+      colour: "#0A0C08",
+    },
+    "04d": {
+      image: require("../../assets/Images/wallpapers/04d.png"),
+      colour: "#ACA8AB",
+    },
+    "04n": {
+      image: require("../../assets/Images/wallpapers/04n.png"),
+      colour: "#46443E",
+    },
+    "09d": {
+      image: require("../../assets/Images/wallpapers/09d.png"),
+      colour: "#5F2C08",
+    },
+    "09n": {
+      image: require("../../assets/Images/wallpapers/09n.png"),
+      colour: "#979797",
+    },
+    "10d": {
+      image: require("../../assets/Images/wallpapers/10d.png"),
+      colour: "#325759",
+    },
+    "10n": {
+      image: require("../../assets/Images/wallpapers/10n.png"),
+      colour: "#0F1A27",
+    },
+    "11d": {
+      image: require("../../assets/Images/wallpapers/11d.png"),
+      colour: "#22222A",
+    },
+    "11n": {
+      image: require("../../assets/Images/wallpapers/11n.png"),
+      colour: "#060609",
+    },
+    "13d": {
+      image: require("../../assets/Images/wallpapers/13d.png"),
+      colour: "#D5D9DF",
+    },
+    "13n": {
+      image: require("../../assets/Images/wallpapers/13n.png"),
+      colour: "#061C3F",
+    },
+    "50d": {
+      image: require("../../assets/Images/wallpapers/50d.png"),
+      colour: "#D5C9BE",
+    },
+    "50n": {
+      image: require("../../assets/Images/wallpapers/50n.png"),
+      colour: "#070E18",
+    },
+  },
+
+  artisticSky: {
+    "01d": {
+      image: require("../../assets/Images/wallpapers/forest-sunny.png"),
+      colour: "#47ab2f",
+    },
+    "01n": {
+      image: require("../../assets/Images/wallpapers/sea-sunny.png"),
+      colour: "#47ab2f",
+    },
+    "02d": {
+      image: require("../../assets/Images/wallpapers/forest-sunny.png"),
+      colour: "#47ab2f",
+    },
+    "02n": {
+      image: require("../../assets/Images/wallpapers/sea-sunny.png"),
+      colour: "#47ab2f",
+    },
+    "03d": {
+      image: require("../../assets/Images/wallpapers/forest-cloudy.png"),
+      colour: "#54717a",
+    },
+    "03n": {
+      image: require("../../assets/Images/wallpapers/sea-cloudy.png"),
+      colour: "#54717a",
+    },
+    "04d": {
+      image: require("../../assets/Images/wallpapers/forest-cloudy.png"),
+      colour: "#54717a",
+    },
+    "04n": {
+      image: require("../../assets/Images/wallpapers/sea-cloudy.png"),
+      colour: "#54717a",
+    },
+    "09d": {
+      image: require("../../assets/Images/wallpapers/forest-rainy.png"),
+      colour: "#57575d",
+    },
+    "09n": {
+      image: require("../../assets/Images/wallpapers/sea-rainy.png"),
+      colour: "#57575d",
+    },
+    "10d": {
+      image: require("../../assets/Images/wallpapers/forest-rainy.png"),
+      colour: "#57575d",
+    },
+    "10n": {
+      image: require("../../assets/Images/wallpapers/sea-rainy.png"),
+      colour: "#57575d",
+    },
+    "11d": {
+      image: require("../../assets/Images/wallpapers/forest-rainy.png"),
+      colour: "#57575d",
+    },
+    "11n": {
+      image: require("../../assets/Images/wallpapers/sea-rainy.png"),
+      colour: "#57575d",
+    },
+    "13d": {
+      image: require("../../assets/Images/wallpapers/forest-cloudy.png"),
+      colour: "#54717a",
+    },
+    "13n": {
+      image: require("../../assets/Images/wallpapers/sea-cloudy.png"),
+      colour: "#54717a",
+    },
+    "50d": {
+      image: require("../../assets/Images/wallpapers/forest-cloudy.png"),
+      colour: "#54717a",
+    },
+    "50n": {
+      image: require("../../assets/Images/wallpapers/sea-cloudy.png"),
+      colour: "#54717a",
+    },
+  },
+  monoChrome: {
+    "01d": {
+      image: null,
+      colour: "#633C0F",
+    },
+    "01n": {
+      image: null,
+      colour: "#060606",
+    },
+    "02d": {
+      image: null,
+      colour: "#74C0F7",
+    },
+    "02n": {
+      image: null,
+      colour: "#05101C",
+    },
+    "03d": {
+      image: null,
+      colour: "#659BD7",
+    },
+    "03n": {
+      image: null,
+      colour: "#0A0C08",
+    },
+    "04d": {
+      image: null,
+      colour: "#ACA8AB",
+    },
+    "04n": {
+      image: null,
+      colour: "#46443E",
+    },
+    "09d": {
+      image: null,
+      colour: "#5F2C08",
+    },
+    "09n": {
+      image: null,
+      colour: "#979797",
+    },
+    "10d": {
+      image: null,
+      colour: "#325759",
+    },
+    "10n": {
+      image: null,
+      colour: "#0F1A27",
+    },
+    "11d": {
+      image: null,
+      colour: "#22222A",
+    },
+    "11n": {
+      image: null,
+      colour: "#060609",
+    },
+    "13d": {
+      image: null,
+      colour: "#D5D9DF",
+    },
+    "13n": {
+      image: null,
+      colour: "#061C3F",
+    },
+    "50d": {
+      image: null,
+      colour: "#D5C9BE",
+    },
+    "50n": {
+      image: null,
+      colour: "#070E18",
     },
   },
 };
